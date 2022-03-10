4257b273d1fb8a8d2e10702bc424605d79499cfc
charlesmariga <charlesmariga37@gmail.com>
2022-03-10 20:54:15 +0300

    Rectified all betty errors in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (void) {
-  puts("Programming is like building a multilingual puzzle\n");
-  return 0;
+/**
+ * main - Prints text to the console
+ *
+ * Description - prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
