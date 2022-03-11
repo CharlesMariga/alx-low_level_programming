52961ad190ae4045bb3570cb63fc5b36e73ceca9
charlesmariga <charlesmariga37@gmail.com>
2022-03-11 19:47:53 +0300

    Removed mixed declarations

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,11 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = (int) n % 10;
+	last_digit = (int) n % 10;
 
 	if (last_digit > 5)
 	{
