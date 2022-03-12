8c0d8194fd2029d38e299d3edcda67512c098d8b
charlesmariga <charlesmariga37@gmail.com>
2022-03-12 05:06:54 +0300

    Modified 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int i, j, k, nums;
+	int i, j, k, l, nums;
 
 	nums = 0;
 
@@ -17,20 +17,34 @@ int main(void)
 	{
 		for (j = 48; j <= 57; j++)
 		{
-			for (k = j + 1; k <= 57; k++)
+			for (k = 48; k <= 57; k++)
 			{
-				if (nums != 0)
+				for (l = 48; l <= 57; l++)
 				{
-					putchar(44);
-					putchar(32);
-				}
+					int numI = i - 48;
+					int numJ = j - 48;
+					int numK = k - 48;
+					int numL = l - 48;
+					
+					int current = (i * 1000) + (j * 100) + (k * 10) + l;
+					int perm = (k * 1000) + (l * 100) + (i * 10) + j;
+					
+					if (current < perm)
+					{
+						if (nums != 0)
+						{
+							putchar(44);
+							putchar(32);
+						}
 
-				putchar(i);
-				putchar(j);
-				putchar(32);
-				putchar(i);
-				putchar(k);
-				nums++;
+						putchar(i);
+						putchar(j);
+						putchar(32);
+						putchar(k);
+						putchar(l);
+						nums++;
+					}
+				}
 			}
 		}
 	}
