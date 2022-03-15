cf34f0ae667d3ce55b5d691916c24280fda84a72
charlesmariga <charlesmariga37@gmail.com>
2022-03-15 17:20:11 +0300

    Refactored 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints alphabets
+ * print_alphabet - prints alphabets
  *
  * Description: prints the alphabets in lowercase
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	int i = 97;
 
@@ -17,6 +17,4 @@ int main(void)
 		i++;
 	}
 	_putchar('\n');
-
-	return (0);
 }
