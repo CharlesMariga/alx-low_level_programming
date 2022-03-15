dda6df054ac6ee6b720146e68e84f894a5188df6
charlesmariga <charlesmariga37@gmail.com>
2022-03-16 00:25:45 +0300

    Refactored 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,20 +10,13 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
-	if (n == 0)
+	if (last_digit < 0)
 	{
-		_putchar('0');
-		return (n);
-	} else if (n < 0)
-	{
-		last_digit = -n % 10;
-		_putchar(last_digit + '0');
-		return (last_digit);
+		last_digit = -last_digit;
 	}
 
-	last_digit = n % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
