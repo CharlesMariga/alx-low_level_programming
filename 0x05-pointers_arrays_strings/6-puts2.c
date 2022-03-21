5bff28dd3ecda551e1378e86d7657be6a86f4f8a
charlesmariga <charlesmariga37@gmail.com>
2022-03-21 19:01:04 +0300

    printing a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other characters of a string
+ * @str: string to be printed
+ *
+ * Description: prints every other character of a string
+ * starting with the first character
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	do {
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i++;
+	} while (*(str + i) != 0);
+	_putchar('\n');
+}
