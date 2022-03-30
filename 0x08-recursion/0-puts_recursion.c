f1952e643f43149edb0ec6c7ffd6342291155b6c
charlesmariga <charlesmariga37@gmail.com>
2022-03-30 20:41:38 +0300

    Edited 0-puts_recursion.c file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,11 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-
-	if (*(s + 1) != '\0')
-	{
-		_puts_recursion(s + 1);
-	} else
-	{
+	if (*s == '\0')
 		_putchar('\n');
-	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	} 
 }
