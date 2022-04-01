e3b2dac3b9b69eb8ee9f3024a478ca8b8b06ba2c
charlesmariga <charlesmariga37@gmail.com>
2022-04-01 19:19:14 +0300

    Modified 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
+/**
+ * is_number - checks to see if input is a number
+ * @s: input to check for numberhood
+ * Return: 1 if it is a number, 0 if not
+ */
+
+int is_number(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
 
 /**
  * main - adds positive numbers
@@ -24,7 +48,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) <= 0)
+		if (!is_number(argv[i]))
 		{
 			printf("Error\n");
 			return (0);
