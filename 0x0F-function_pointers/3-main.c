f2993b22713320d13dc43b7ab5b5f70a7fd0da9e
charlesmariga <charlesmariga37@gmail.com>
2022-04-13 18:36:42 +0300

    Modified 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,8 +13,8 @@
  */
 int main(int argc, char **argv)
 {
-	char *operations[5] = {"+", "-", "*", "/", "%"};
-	int i = 0, index = -1;
+	int a, b, result;
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
@@ -22,22 +22,18 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (argv[2][1] == '\0')
-	{
-		for (; i < 5; i++)
-		{
-			if (argv[2][0] == operations[i][0])
-				index = i;
-		}
-	}
-
-	if (index == -1)
+	op = get_op_func(argv[2]);
+	if (op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = op(a, b);
+
+	printf("%d\n", result);
 
 	return (0);
 }
