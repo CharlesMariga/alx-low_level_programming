950b29738ba7b41c937da36b877b98ed3d83e710
charlesmariga <charlesmariga37@gmail.com>
2022-04-15 00:39:33 +0300

    Modified 2-print_strings.c file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,6 +2,43 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_string - prints a string
+ * @separator: string separator
+ * @n: number of string arguments
+ * @i: index
+ * @s: string
+ *
+ * Descripton: prints a string
+ *
+ * Return: void
+ */
+void print_string(const char *separator,
+unsigned int n, unsigned int i, char *s)
+{
+	if (separator != NULL)
+	{
+		if (i == n - 1 && s == NULL)
+			printf("(nil)\n");
+		else if (i == n - 1 && s != NULL)
+			printf("%s\n", s);
+		else if (n != n - 1 && s == NULL)
+			printf("(nil)%s", separator);
+		else if (n != n - 1 && s != NULL)
+			printf("%s%s", s, separator);
+	} else
+	{
+		if (i == n - 1 && s == NULL)
+			printf("(nil)\n");
+		else if (i == n - 1 && s != NULL)
+			printf("%s\n", s);
+		else if (n != n - 1 && s == NULL)
+			printf("(nill)");
+		else if (n != n - 1 && s != NULL)
+			printf("%s", s);
+	}
+}
+
 
 /**
  * print_strings - prints strings
@@ -29,18 +66,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
-		if (separator != NULL)
-		{
-			if (i == n - 1)
-				printf("%s\n", s);
-			else
-				printf("%s%s", s, separator);
-		} else
-		{
-			if (i == n - 1)
-				printf("%s\n", s);
-			else
-				printf("%s", s);
-		}
+		print_string(separator, n, i, s);
 	}
 }
