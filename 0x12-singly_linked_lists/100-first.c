77311687af3367efe6b8817481d64b0ff578813e
charlesmariga <charlesmariga37@gmail.com>
2022-04-22 02:06:41 +0300

    Modified 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@
  */
 void __attribute__ ((constructor)) premain()
 {
-	printf("%s %s",
-	"You're beat! and yet, you must allow,\n",
-	"I bore my house upon my back!\n");
+	printf(
+	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+	);
 }
