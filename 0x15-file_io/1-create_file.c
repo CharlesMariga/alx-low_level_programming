cbc5c18658ad0ae6b03a33795b288616b44439b3
charlesmariga <charlesmariga37@gmail.com>
2022-05-03 18:38:32 +0300

    Edited 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IWUSR, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -31,7 +31,10 @@ int create_file(const char *filename, char *text_content)
 
 		write_status = write(fd, text_content, textCount);
 		if (write_status == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 	}
 
 	close(fd);
