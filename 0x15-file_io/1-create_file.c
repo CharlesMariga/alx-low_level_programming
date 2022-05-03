0f38ee416bcb543370fa14543f5d13fd5125f78e
charlesmariga <charlesmariga37@gmail.com>
2022-05-03 20:29:41 +0300

    Edited 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,17 +24,19 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (textCount)
+	if (!textCount)
 	{
-		while (*(text_content + textCount))
-			textCount++;
+		text_content = "";
+	}
 
-		write_status = write(fd, text_content, textCount);
-		if (write_status == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+	while (*(text_content + textCount))
+		textCount++;
+
+	write_status = write(fd, text_content, textCount);
+	if (write_status == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
